287faec09b5ca89e53dc752480e72dcd1faab959
Jacob Mendoza <jacob.mendoza.hdz@gmail.com>
2023-03-10 22:30:36 -0600

    initial

---
+++ apisJacob/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apisJacob.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ apisJacob/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="apisJacob.WebApiApplication" Language="C#" %>
